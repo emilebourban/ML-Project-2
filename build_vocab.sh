6eb050582dfe1c58e46aa9561c9b9c14099cad99
emilebourban <emilebourban@gmail.com>
2018-12-06 22:27:10 +0100

    Delete build_vocab.sh

--- build_vocab.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Note that this script uses GNU-style sed. On Mac OS, you are required to first
-#    brew install gnu-sed --with-default-names
-cat twitter-datasets/pos_train.txt twitter-datasets/neg_train.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > twitter-datasets/vocab.txt
